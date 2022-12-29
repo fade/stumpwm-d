a4c0194625db36c135e5a3b9c6708acd36f3faee
Brian O'Reilly <fade@deepsky.com>
2022-12-29 10:40:03 -0500

    some early init stuff which will be moved down to inferred systems.

--- init.lisp
+++ init.lisp
@@ -1,7 +1,23 @@
 (cl:in-package :stumpwm-user)
-(require :quicklisp)
 
-(ql:quickload :stumpwm-init)
+;;; ======================================================================
+;;; if quicklisp isn't loaded, load it.
+;;; ======================================================================
+
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (message "Loading/Using quicklisp, slynk etc....")
+    (load quicklisp-init)))
+
+(use-package :ql)
+
+;; common lisp string processing lib.
+(quickload "str")
+(quickload "cl-mount-info")
+
+(quickload :stumpwm-init)
 
 ;;; ======================================================================
 ;;; Start a slynk server so we can modify stump as it runs, from emacs
@@ -21,13 +37,13 @@
 
 (defvar *slynk-p* nil)
 (if *slynk-p*
-      (message "Slynk server is already running.")
-      (progn
-	(slynk:create-server :port 4006
-			     :style slynk:*communication-style*
-			     :dont-close t)
-	(setf *slynk-p* t)
-	(message "Slynk server started.")))
+    (message "Slynk server is already running.")
+  (progn
+    (slynk:create-server :port 4006
+			 :style slynk:*communication-style*
+			 :dont-close t)
+    (setf *slynk-p* t)
+    (message "Slynk server started.")))
 
 ;;; ======================================================================
 ;;  init the lisp listener.
@@ -35,3 +51,32 @@
 
 (in-package :stumpwm)
 (setf *default-package* :stumpwm)
+
+
+;;; ======================================================================
+;;; too many things use notifications for core functionality to ignore
+;;; them. This almost always works at least 80% of the time.
+;;; ======================================================================
+
+(load-module "notify")
+
+(defvar *notifications* nil)
+
+(if (and (find-package :notify) (not *notifications*))
+    (progn
+      (notify:notify-server-toggle)
+      (setf *notifications* t))
+    (message "The notify package was not loaded. It is already present."))
+
+;;; ======================================================================
+;;;  group definitions
+;;; ======================================================================
+
+;; set the name of the initial group, which otherwise defaults to "Default"
+(setf (group-name (first (screen-groups (current-screen)))) "Terminal")
+
+(dolist (space (list "Emacs" "Music" "Mail" "Boomer" "StumpConfig" "Nish" "Kleevo"))
+  (gnewbg space))
+
+;; what to do with the mouse?
+(setf *mouse-focus-policy* :sloppy)
