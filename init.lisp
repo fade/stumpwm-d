a54b61e6374302ae91d34af837fe6c4464426f44
Brian O'Reilly <fade@deepsky.com>
2022-12-29 13:33:46 -0500

    let us work in the :stumpwm package

--- init.lisp
+++ init.lisp
@@ -1,4 +1,7 @@
-(cl:in-package :stumpwm-user)
+(cl:in-package :stumpwm)
+
+(setf *debug-level* 3)
+(redirect-all-output (data-dir-file "debug-output" "txt"))
 
 ;;; ======================================================================
 ;;; if quicklisp isn't loaded, load it.
@@ -11,13 +14,13 @@
     (message "Loading/Using quicklisp, slynk etc....")
     (load quicklisp-init)))
 
-(use-package :ql)
+;; (use-package :ql)
 
 ;; common lisp string processing lib.
-(quickload "str")
-(quickload "cl-mount-info")
+(asdf:load-system "str")
+(asdf:load-system "cl-mount-info")
 
-(quickload :stumpwm-init)
+;; (asdf:load-system :stumpwm-init)
 
 ;;; ======================================================================
 ;;; Start a slynk server so we can modify stump as it runs, from emacs
@@ -27,7 +30,7 @@
 ;; that stump was built with matches the lisp image we are using in
 ;; general for development, or you will get gnarly tracebacks.
 
-(quickload 'slynk)
+(asdf:load-system 'slynk)
 
 (setf stumpwm:*top-level-error-action* :break)
 
@@ -36,15 +39,15 @@
 ;;; ======================================================================
 
 (defvar *slynk-p* nil)
+
 (if *slynk-p*
     (message "Slynk server is already running.")
-  (progn
-    (slynk:create-server :port 4006
-			 :style slynk:*communication-style*
-			 :dont-close t)
-    (setf *slynk-p* t)
-    (message "Slynk server started.")))
-
+    (progn
+      (slynk:create-server :port 4006
+			   :style slynk:*communication-style*
+			   :dont-close t)
+      (setf *slynk-p* t)
+      (message "Slynk server started.")))
 ;;; ======================================================================
 ;;  init the lisp listener.
 ;;; ======================================================================
@@ -58,25 +61,27 @@
 ;;; them. This almost always works at least 80% of the time.
 ;;; ======================================================================
 
-(load-module "notify")
+;; (load-module "notify")
 
-(defvar *notifications* nil)
+;; (defvar *notifications* nil)
 
-(if (and (find-package :notify) (not *notifications*))
-    (progn
-      (notify:notify-server-toggle)
-      (setf *notifications* t))
-    (message "The notify package was not loaded. It is already present."))
+;; (if (and (find-package :notify) (not *notifications*))
+;;     (progn
+;;       (notify:notify-server-toggle)
+;;       (setf *notifications* t))
+;;     (message "The notify package was not loaded. It is already present."))
 
 ;;; ======================================================================
 ;;;  group definitions
 ;;; ======================================================================
 
-;; set the name of the initial group, which otherwise defaults to "Default"
-(setf (group-name (first (screen-groups (current-screen)))) "Terminal")
+;; ;; set the name of the initial group, which otherwise defaults to "Default"
+;; (setf (group-name (first (screen-groups (current-screen)))) "Terminal")
 
-(dolist (space (list "Emacs" "Music" "Mail" "Boomer" "StumpConfig" "Nish" "Kleevo"))
-  (gnewbg space))
+;; (dolist (space (list "Emacs" "Music" "Mail" "Boomer" "StumpConfig" "Nish" "Kleevo"))
+;;   (gnewbg space))
 
-;; what to do with the mouse?
-(setf *mouse-focus-policy* :sloppy)
+;; ;; what to do with the mouse?
+;; (setf *mouse-focus-policy* :sloppy)
+
+(asdf:load-system :stumpwm-init)
