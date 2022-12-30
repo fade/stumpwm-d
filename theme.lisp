a77b67a61911d4e17ed4a58585df2a33955a028a
Brian O'Reilly <fade@deepsky.com>
2022-12-30 15:43:32 -0500

    make the colour theme stick.

--- theme.lisp
+++ theme.lisp
@@ -2,7 +2,10 @@
   (:use :cl)
   (:import-from :stumpwm-init/deepsky)
   (:import-from :stumpwm
-                #:*colors*
+                ;; #:*colors*
+                #:kbd
+                #:define-key
+                #:defcommand
                 #:set-fg-color
                 #:set-bg-color
                 #:set-border-color
@@ -13,7 +16,10 @@
                 #:*mode-line-foreground-color*
                 #:*mode-line-background-color*
                 #:*mode-line-border-color*
-                #:run-shell-command))
+                #:run-shell-command)
+  (:export #:*desktop-background-command*
+           #:*background-images*
+           #:backgrounds))
 
 ;; ensure this file is loaded before modeline.lisp because otherwise
 ;; the modeline may not respect the colors bound below.
@@ -23,23 +29,23 @@
 ;; the working colour palette.
 
 ;;; As per commentary in color.lisp
-(defparameter *colors* `(deepsky:background        ; ^0
-                         deepsky:foreground        ; ^1
-                         deepsky:current-line      ; ^2
-                         deepsky:cloudless-day     ; ^3
-                         deepsky:bluebird          ; ^4
-                         deepsky:snow              ; ^5
-                         deepsky:white             ; ^6
-                         deepsky:peachpuff         ; ^7
-                         deepsky:lemonchiffon      ; ^8
-                         deepsky:comment
-                         deepsky:cyan
-                         deepsky:green
-                         deepsky:orange
-                         deepsky:pink
-                         deepsky:purple
-                         deepsky:red
-                         deepsky:yellow))
+(setf stumpwm:*colors* `(,deepsky:background        ; ^0
+                         ,deepsky:foreground        ; ^1
+                         ,deepsky:current-line      ; ^2
+                         ,deepsky:cloudless-day     ; ^3
+                         ,deepsky:bluebird          ; ^4
+                         ,deepsky:snow              ; ^5
+                         ,deepsky:white             ; ^6
+                         ,deepsky:peachpuff         ; ^7
+                         ,deepsky:lemonchiffon      ; ^8
+                         ,deepsky:comment
+                         ,deepsky:cyan
+                         ,deepsky:green
+                         ,deepsky:orange
+                         ,deepsky:pink
+                         ,deepsky:purple
+                         ,deepsky:red
+                         ,deepsky:yellow))
 
 (update-color-map (current-screen))
 ;; (when *initializing*
@@ -77,3 +83,27 @@
   )
 
 (stumpwm:add-hook stumpwm:*start-hook* 'set-theme)
+
+;;; ======================================================================
+;;; stumpwm aesthetic configuration.
+;;; ======================================================================
+
+(defparameter *background-images* (list "$HOME/Desktop_pics/dpool/" "$HOME/Desktop_pics/Cityscapes/*") ;; "$HOME/Desktop_pics/Desktop_Images_Night_City/"
+  "Directory containing desktop wallpapers.")
+
+(defparameter *desktop-background-command* (format nil "feh --randomize --bg-scale ~{~A~^ ~}" *background-images*)
+  "command to set the desktop wallpaper, as a string.")
+
+;; set the background image when this init file is loaded. Obviously,
+;; this assumes that the image directory path exists. If your image
+;; files are stored on an external drive connected to USB, this will
+;; likely cause a delay at this point loading the initfile.
+(run-shell-command *desktop-background-command*)
+
+;; command to set the desktop background at will.
+(defcommand backgrounds () ()
+  "set the root window backgrounds for the attached heads."
+  (run-shell-command *desktop-background-command*))
+
+;; bind the backgrounds command to a key in the root map.
+(define-key stumpwm:*root-map* (kbd "F9") "backgrounds")
