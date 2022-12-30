2a5dfd0493ab4d66976d53872b1e5d3f707f264a
Brian O'Reilly <fade@deepsky.com>
2022-12-30 15:44:13 -0500

    a place for personal keybindings to live.

---
+++ personal-keys.lisp
@@ -0,0 +1,34 @@
+(uiop:define-package :stumpwm-init/personal-keys
+  (:import-from :stumpwm-init/keybinding-macros
+                #:super-key-maps
+                #:windowed-app-launcher
+                #:s-
+                #:s-l
+                #:bind))
+
+
+(in-package :stumpwm-init/personal-keys)
+
+;;;----------------------------------------------------------------------
+;;; keybinds
+;;;----------------------------------------------------------------------
+
+;; Nyxt is a keyboard driven web browser written in common lisp.
+
+;; (defcommand nyxt () ()
+;;   "run or raise the nyxt key-driven webbrowser."
+;;   (run-or-raise "nyxt" '(:class "nyxt")))
+
+;; b for browser
+(windowed-app-launcher nyxt b)
+;; f for real browser
+(windowed-app-launcher firefox f)
+
+;; m for music
+(windowed-app-launcher clementine m)
+
+
+;; c for light/fast terminal
+(windowed-app-launcher kitty c)
+;; C for reliable URL recog (konsole)
+(windowed-app-launcher konsole o)
